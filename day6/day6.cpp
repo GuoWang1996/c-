7ef2f69ffb3fab7d035c79ab449f7956db2bd892
wang guo <2431605485@qq.com>
2022-08-31 09:18:41 +0800

    hfgh

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -6,13 +6,20 @@ using namespace std;
 class Person
 {
 public:
+	int age;
 	virtual void speak() {
 		printf("我是Person!\n");
+	}
+	virtual void eat() {
+		printf("Person在eat!\n");
 	}
 	Person()
 	{
 	}
-
+	Person(int age)
+	{
+		this->age = age;
+	}
 	~Person()
 	{
 	}
@@ -30,40 +37,34 @@ public:
 	 void eat() {
 		printf("学生在eat!\n");
 	}
+	 Student(int age)
+	 {
+		 this->age = age;
+	 }
 };
 class Techer :public Person
 {
 public:
 
-	void speak() {
+	void eat() {
 		printf("我是Techer@!\n");
 	}
 };
 void doWork(Person& p)
 {
-	p.speak();
+	p.eat();
 }
-
+Techer t;
+Student s(18);
 void test01()
 {
-	Techer t;
-	Student s;
+	
 	doWork(t);
 	doWork(s);
 }
+
 int main()
 {
 	test01();
 	system("pause");
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
